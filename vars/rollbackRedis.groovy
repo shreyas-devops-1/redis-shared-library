aa0e1236a69e09a146a86fec0343f007c1c33ed1
Shreyas Awasthi <shreyasawasthi398@gmail.com>
2026-05-01 16:24:33 +0530

    Update rollbackRedis.groovy

--- vars/rollbackRedis.groovy
+++ vars/rollbackRedis.groovy
@@ -3,8 +3,8 @@ import org.opstree.AnsibleRunner
 def call(Map config) {
     def runner = new AnsibleRunner(this)
     runner.adHoc(
-        'role_middleware',
+        'localhost',
         config.inventory,
-        'test -f /etc/redis/redis.conf.bak && cp /etc/redis/redis.conf.bak /etc/redis/redis.conf && systemctl restart redis-server && /usr/bin/redis-cli ping || echo "No backup found, skipping rollback"'
+        'test -f /etc/redis/redis.conf.bak && cp /etc/redis/redis.conf.bak /etc/redis/redis.conf && systemctl restart redis-server || true'
     )
 }
