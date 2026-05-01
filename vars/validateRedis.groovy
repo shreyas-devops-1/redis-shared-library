38b134be623052d1a7d5f4ae899a6202d7f38e87
Shreyas Awasthi <shreyasawasthi398@gmail.com>
2026-05-01 16:25:14 +0530

    Update validateRedis.groovy

--- vars/validateRedis.groovy
+++ vars/validateRedis.groovy
@@ -3,7 +3,7 @@ import org.opstree.AnsibleRunner
 def call(Map config) {
     def runner = new AnsibleRunner(this)
     runner.adHoc(
-        'role_middleware',
+        'localhost',
         config.inventory,
         '/usr/bin/redis-cli -p 6379 ping | grep -q PONG && echo "Redis healthy" || exit 1'
     )
