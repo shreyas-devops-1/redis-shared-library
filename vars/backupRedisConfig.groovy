ffde3a6d220efd96400497b41384f6ff91d81f91
Shreyas Awasthi <shreyasawasthi398@gmail.com>
2026-05-01 16:23:35 +0530

    Update backupRedisConfig.groovy

--- vars/backupRedisConfig.groovy
+++ vars/backupRedisConfig.groovy
@@ -3,8 +3,9 @@ import org.opstree.AnsibleRunner
 def call(Map config) {
     def runner = new AnsibleRunner(this)
     runner.adHoc(
-        'role_middleware',
+        'localhost',
         config.inventory,
-        'test -f /etc/redis/redis.conf && cp /etc/redis/redis.conf /etc/redis/redis.conf.bak || echo "No existing config, skipping backup"'
+        'test -f /etc/redis/redis.conf && cp /etc/redis/redis.conf /etc/redis/redis.conf.bak || true'
     )
 }
+
