5f547786a41bb8fca92a14e97a5fc5d46cf30cd4
Shreyas Awasthi <shreyasawasthi398@gmail.com>
2026-05-02 14:16:20 +0530

    Delete vars directory

--- vars/dryRunRedis.groovy
+++
@@ -1,6 +0,0 @@
-import org.opstree.AnsibleRunner
-
-def call(Map config) {
-    def runner = new AnsibleRunner(this)
-    runner.dryRun(config.playbook, config.inventory)
-}
--- vars/syntaxCheckRedis.groovy
+++
@@ -1,6 +0,0 @@
-import org.opstree.AnsibleRunner
-
-def call(Map config) {
-    def runner = new AnsibleRunner(this)
-    runner.syntaxCheck(config.playbook, config.inventory)
-}
